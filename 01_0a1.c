ce1968b6b392843b728f405c399d500daca846eb
Candra <26895295+Caichang456@users.noreply.github.com>
2020-08-02 18:43:50 +0700

    Add files via upload

---
+++ 01_0a1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	printf("\n======================================================================\n");
+	printf("\t\t\tInformasi Matakuliah");
+	printf("\n======================================================================\n\n");
+	printf("Kode Mata Kuliah\t: MKB222\t\tJenjang Studi\t: S1\n");
+	printf("Nama Mata Kuliah\t: Bahasa Pemrograman I\tProgram Studi\t: TI\n");
+	printf("SKS Mata Kuliah\t\t: 2\t\t\tSemester\t: S1\n\n");
+	printf("======================================================================\n");
+	return 0;
+}
